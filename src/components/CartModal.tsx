caa8891b7d7e117cc5d786ada7f15876d635486a
sijibomi <gbemilesanmi@gmail.com>
2023-07-20 17:06:25 +0100

    shed cherish was here.

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -6,12 +6,15 @@ import { RiShoppingCart2Line, RiDeleteBin7Fill } from 'react-icons/ri'
 import { calculateTotalPrice } from '../utils/functions'
 import useProduct from '../context/ProductContext'
 import { removeFromCart } from '../utils/firebaseFunctions'
+import { Button } from '@material-tailwind/react'
+import { useNavigate } from 'react-router-dom'
 
 export default function CartModal() {
     const { isModalOpen, user } = useUserAuth()
     const { setCartLength } = useProduct()
     const [cartt, setCartt] = useState<Product[] | null>(null)
     const [loading, setLoading] = useState(false)
+    const nav = useNavigate()
 
     useEffect(() => {
         const fetchCart = async () => {
@@ -49,7 +52,7 @@ export default function CartModal() {
             <div
                 className={` ${
                     isModalOpen ? null : 'hidden'
-                } w-full h-1/2  lg:w-2/3 lg:h-2/3 bg-[#e0d3b0] lg:rounded-2xl flex flex-col justify-center p-3`}
+                } w-full h-2/3  lg:w-2/3 lg:h-2/3 bg-[#e0d3b0] lg:rounded-2xl flex flex-col p-3 font-rubik`}
             >
                 {/*loading && <p>Loading like a pro ...</p>*/}
                 {cartt?.length! > 0 ? (
@@ -58,14 +61,20 @@ export default function CartModal() {
                     </p>
                 ) : null}
                 {cartt?.length! > 0 ? (
-                    <div className="min-h-[80%] flex flex-col md:flex-row flex-wrap overflow-y-scroll text-black">
-                        <div className=" w-full md:w-1/2 h-1/2 md:h-full flex flex-col gap-2">
+                    <div className="min-h-[80%] flex flex-col items-center gap-10 md:flex-row flex-wrap text-black">
+                        <div className=" w-full md:w-1/2 h-1/2 md:h-full flex flex-col gap-2 overflow-y-scroll py-3">
                             {(cartt as any)?.map((product: Product, index: number) => (
-                                <div key={index} className="text-black border border-black ">
-                                    <div className="grid grid-cols-3 p-2">
-                                        <p>{product.name}</p>
+                                <div key={index} className="text-black  ">
+                                    <div className="grid grid-cols-5 p-2 items-center">
+                                        <div
+                                            style={{
+                                                backgroundImage: `url(${product.imageUrl})`
+                                            }}
+                                            className="w-16 h-16 rounded-[50%] bg-cover bg-center"
+                                        ></div>
+                                        <p className="col-span-2">{product.name}</p>
                                         <p>${product.price}</p>
-                                        <div>
+                                        <div className="flex items-center justify-center">
                                             <RiDeleteBin7Fill
                                                 onClick={() => handleRemoveItem(product.id)}
                                                 className="cursor-pointer "
@@ -75,7 +84,23 @@ export default function CartModal() {
                                 </div>
                             ))}
                         </div>
-                        <div className=" w-full md:w-1/2 h-1/2 md:h-full ">${calculateTotalPrice(cartt!)}</div>
+                        <div className="w-1/3 flex flex-col gap-3">
+                            <i className="fa-solid fa-cart-shopping fa-bounce text-9xl text-center hidden lg:block"></i>
+                            <div className="flex flex-col gap-3">
+                                <div className=" w-full md:w-1/2 h-1/2 md:h-full flex items-center gap-3 justify-center">
+                                    <h5 className="text-xl font-bold">Total:</h5>
+                                    <p>${calculateTotalPrice(cartt!)}</p>
+                                </div>
+                                <Button
+                                    className="bg-black text-white font-rubik py-3 px-12 rounded-md"
+                                    onClick={() => {
+                                        window.location.pathname = '/checkout'
+                                    }}
+                                >
+                                    Checkout
+                                </Button>
+                            </div>
+                        </div>
                     </div>
                 ) : (
                     <div className="min-h-[80%] flex items-center justify-center flex-col gap-2 text-center">
--- src/components/MultiStepForm/Review.tsx
+++ src/components/MultiStepForm/Review.tsx
@@ -3,45 +3,111 @@ import useMulti from '../../context/MultiStepFormContext.tsx'
 import useProduct from '../../context/ProductContext.tsx'
 import useUserAuth from '../../context/AuthContext.tsx'
 import { getUserCart } from '../../utils/firebaseFunctions.ts'
-import { toast } from 'sonner'
+import { Product } from '../../Types/Product.ts'
+import Lottie from 'react-lottie-player'
+import { useNavigate } from 'react-router-dom'
+import { Button } from '@material-tailwind/react'
 
 export default function Review() {
     const { address, setAddress, payment, setPayment } = useMulti()
     const {} = useProduct()
     const [cartt, setCartt] = useState<Product[] | null>(null)
-    const [loading, setLoading] = useState(false)
     const { user } = useUserAuth()
+    const nav = useNavigate()
+    const [modalOpen, setModalOpen] = useState(false)
 
     useEffect(() => {
         const fetchCart = async () => {
             try {
-                setLoading(true)
                 const cart = await getUserCart(user?.uid!)
                 setCartt(cart.items)
             } catch (e) {
                 setCartt(null)
             } finally {
-                setLoading(false)
             }
         }
-
         if (payment && address) {
             fetchCart()
             console.log(cartt)
         }
     }, [address])
 
-    const handleSubmit = (values: typeof address) => {
-        setPayment(null)
-        setAddress(null)
-        console.log('Submitted')
+    const handleSubmit = () => {
+        setTimeout(() => {
+            //clear cart
+            setPayment(null)
+            setAddress(null)
+            console.log('Submitted')
+            nav('/')
+        }, 3000)
     }
 
     return (
         <section className="w-full h-full flex items-center justify-center">
-            <div className="bg-gray-700  text-white w-5/6 md:w-2/3 h-2/3 p-3 rounded-3xl shadow-2xl">
-                <p>{address.city}</p>
-            </div>
+            {modalOpen && (
+                <Lottie
+                    path="https://lottie.host/baf41332-ace5-4dfa-82ca-1c67e3633900/usIEqqZRbn.json"
+                    //mode="bounce"
+                    //background="black"
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        margin: '0 auto'
+                        //minHeight: '400px',
+                        //minWidth: '300px'
+                    }}
+                    loop
+                    play
+                    //onLoopComplete={handleLoop}
+                ></Lottie>
+            )}
+            {!modalOpen && (
+                <div className="bg-gray-700 grid grid-cols-1  md:grid-cols-2 flex-wrap justify-between font-rubik text-white w-5/6 md:w-1/2 h-2/3 p-3 rounded-3xl shadow-2xl items-center ">
+                    <div className=" col-span-1 flex flex-col gap-4 items-center h-full border-black  justify-center ">
+                        <div className="flex flex-col gap-2 w-2/3">
+                            <p className="text-lg font-normal">{user?.displayName}</p>
+                            <p className="text-lg font-light">{user?.email}</p>
+                            <p className="text-lg font-light">{payment?.phoneNumber}</p>
+                            <hr />
+                            <p className="text-xl font-bold">{address.street},</p>
+                            <p className="text-lg font-light">
+                                {address.city}, {address.state}.
+                            </p>
+                            <p className="text-base tracking-widest font-light">{address.postalCode}</p>
+                        </div>
+                        <Button
+                            variant="outlined"
+                            className="bg-black text-white font-rubik py-3 px-12 rounded-md w-2/3"
+                            onClick={() => {
+                                setModalOpen(true)
+                                handleSubmit()
+                            }}
+                        >
+                            Submit
+                        </Button>
+                    </div>
+
+                    <div className=" hidden  col-span-1 h-full md:flex flex-col justify-between items-center gap-3 overflow-scroll">
+                        {cartt?.map((item) => (
+                            <div
+                                key={item.id}
+                                className="grid grid-cols-4  py-2 items-center gap-2 w-full border-b-1 border-b-white"
+                            >
+                                <img
+                                    src={item.imageUrl}
+                                    className="col-span-1"
+                                    style={{ borderRadius: '50%', width: '60px', height: '60px' }}
+                                ></img>
+                                <div className="col-span-3">
+                                    <p>{item.name}</p>
+                                    <p>{item.price}</p>
+                                    {/* <p>{item.quantity}</p> */}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
         </section>
     )
 }
